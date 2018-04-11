2f62fba15240f7ccb3375f3398f220e942971075
peti <petikoch@gmail.com>
2018-04-11 19:14:06 +0200

    simplified question 6 solution

--- src/main/java/question6/client/ClientApplication.java
+++ src/main/java/question6/client/ClientApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -33,10 +31,7 @@ public class ClientApplication {
                     .uri("/question6/millionvalues")
                     .retrieve()
                     .bodyToFlux(String.class)
-                    .take(1_000_000)
-                    .zipWith(
-                            Flux.interval(Duration.ofMillis(100))
-                    ).map(Tuple2::getT1)
+                    .delayElements(Duration.ofMillis(100))
                     .subscribe(
                             System.out::println,
                             Throwable::printStackTrace,
